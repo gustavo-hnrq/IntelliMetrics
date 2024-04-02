b4b342df0b03077f44e3a228ded9ada29f9cbc3b
SophiaSantos17 <santosvsophia17@gmail.com>
2024-04-02 16:32:55 -0300

    UPDATE: Página add relatório comentada

--- src/pages/add-relatorio.jsx
+++ src/pages/add-relatorio.jsx
@@ -6,25 +6,31 @@ import { Label } from "@/components/ui/label";
 import { useState } from "react";
 
 export default function AddRelatorio() {
+  // // Define um estado 'linhas' que inicialmente contém uma matriz com um objeto representando uma linha vazia.
   const [linhas, setLinhas] = useState([
     {
       id: 1,
-      valores: ["", "", "", "", "", "", "", ""],
+      valores: ["", "", "", "", "", "", "", ""], // Array de strings vazias representando os valores da linha
     },
   ]);
 
+  // Define um estado 'inputs' que inicialmente contém um array com duas strings vazias.
   const [inputs, setInputs] = useState(["", ""]);
 
+  // Função para adicionar um novo elemento vazio ao array 'inputs'
   const adicionarMetrologistas = () => {
-    // Clonar o array inputs e adicionar um novo elemento vazio
-    setInputs([...inputs, '']);
-  }
+    // Clona o array 'inputs' existente e adiciona uma nova string vazia ao final
+    setInputs([...inputs, ""]);
+  };
 
+  // Função para adicionar uma nova linha ao array 'linhas'
   const adicionarLinha = () => {
+    // Cria uma nova linha com um 'id' incrementado em relação ao número de linhas existentes e valores iniciais vazios.
     const novaLinha = {
-      id: linhas.length + 1,
-      valores: ["", "", "", "", "", "", "", ""],
+      id: linhas.length + 1, // Incrementa o 'id' para manter a unicidade
+      valores: ["", "", "", "", "", "", "", ""], // Array de strings vazias representando os valores da linha
     };
+    // Clona o array 'linhas' existente e adiciona a nova linha ao final
     setLinhas([...linhas, novaLinha]);
   };
 
@@ -51,6 +57,7 @@ export default function AddRelatorio() {
                 METROLOGISTAS
               </h4>
               <div className="flex flex-wrap w-full justify-between gap-3">
+                {/* .map QUE ADICIONA A QUANTIDADE QUE O USUÁRIO QUEISER DE METROLOGISTAS */}
                 {inputs.map((input, index) => (
                   <Input
                     key={index}
@@ -70,6 +77,7 @@ export default function AddRelatorio() {
 
             {/* CONDIÇÕES AMBIENTAIS */}
             <div>
+              {/* TITULO */}
               <h4 className="text-black font-extrabold text-base w-full py-3 px-4 bg-[#DBDBDB] uppercase">
                 condições ambientais
               </h4>
@@ -93,6 +101,7 @@ export default function AddRelatorio() {
 
             {/* INSTURMENTO DE MEDIÇÃO */}
             <div>
+              {/* TITULO */}
               <h4 className="text-black font-extrabold text-base w-full py-3 px-4 bg-[#DBDBDB] uppercase">
                 instumento de medição
               </h4>
@@ -144,6 +153,7 @@ export default function AddRelatorio() {
 
             {/* CERTIFICADO DE CALIBRAÇÃO DO INSTRUMENTO  */}
             <div>
+              {/* TITULO */}
               <h4 className="text-black font-extrabold text-base w-full py-3 px-4 bg-[#DBDBDB] uppercase">
                 certificado de calibração do instrumento
               </h4>
@@ -174,6 +184,7 @@ export default function AddRelatorio() {
 
             {/* OBJETO DE MEDIÇÃO */}
             <div>
+              {/* TITULO */}
               <h4 className="text-black font-extrabold text-base w-full py-3 px-4 bg-[#DBDBDB] uppercase">
                 objeto de medição
               </h4>
@@ -230,7 +241,9 @@ export default function AddRelatorio() {
               </div>
             </div>
 
+            {/* MENSURADOS */}
             <div>
+              {/* TITULO */}
               <h4 className="text-black font-extrabold text-base w-full py-3 px-4 bg-[#DBDBDB] uppercase">
                 mensurados(m)
               </h4>
@@ -238,6 +251,7 @@ export default function AddRelatorio() {
               {/* tabela */}
               <div className="overflow-x-auto w-full py-3 px-4">
                 <table className="min-w-full border-collapse border border-black rounded-lg">
+                  {/* TITULO DA TABELA */}
                   <thead className="bg-[#DBDBDB]">
                     <tr>
                       <th
@@ -290,6 +304,7 @@ export default function AddRelatorio() {
                       </th>
                     </tr>
                   </thead>
+                  {/* CORPO DA TABELA */}
                   <tbody className="bg-white border-collapse border border-black">
                     {linhas.map((linha, index) => (
                       <tr key={linha.id}>
@@ -310,10 +325,12 @@ export default function AddRelatorio() {
                     ))}
                   </tbody>
                 </table>
+                {/* BOTÃO ONDE ELE ADICIONA QUANTAS LINHAS FOR NECESSÁRIAS */}
                 <div className="w-full flex flex-row justify-end items-center py-5 gap-3">
                   <Button onClick={adicionarLinha}>Adicionar Linha</Button>
                 </div>
               </div>
+              {/* INUPUTS */}
               <div>
                 <div className="flex flex-col w-full py-3 px-4">
                   <Label className="text-[#3F3F3F] font-extrabold text-base w-full py-1 px-4 bg-[#DBDBDB] rounded-t-lg">
@@ -326,6 +343,7 @@ export default function AddRelatorio() {
                   />
                 </div>
 
+                {/* COLUNA DE INPUTS */}
                 <div className="flex flex-row w-full justify-between py-3 gap-7 px-4">
                   <div className="flex flex-row gap-3 w-[30%] items-center">
                     <Label className="font-bold text-[#828282] text-base w-[70%]">
@@ -346,6 +364,8 @@ export default function AddRelatorio() {
                       className="placeholder:text-[#828282] font-bold"
                     />
                   </div>
+
+                  {/* COLUNA DE INPUTS */}
                   <div className="flex flex-row gap-3 w-[30%] items-center">
                     <Label className="font-bold text-[#828282] text-base w-[70%]">
                       Termino:
@@ -365,6 +385,7 @@ export default function AddRelatorio() {
                       className="placeholder:text-[#828282] font-bold"
                     />
                   </div>
+                  {/* COLUNA DE INPUTS */}
                   <div className="flex flex-row gap-3 w-[30%] items-center">
                     <Label className="font-bold text-[#828282] text-base w-[500px]">
                       Tempo Total:
@@ -416,6 +437,7 @@ export default function AddRelatorio() {
               </div>
             </div>
           </div>
+          {/* BOTÕES DO FORMULÁRIO */}
           <div className="w-[90%] flex flex-row justify-end items-center py-5 gap-3">
             <Button className="w-[200px]">Adicionar</Button>
             <Button className="w-[200px] border-[#858585] border-2 bg-transparent text-[#949494] font-semibold hover:bg-[#858585] hover:text-white">
