46d327fd0b249c120d38e15c88497a40ef7ad47c
MelissaaGomes <131712480+MelissaaGomes@users.noreply.github.com>
2024-05-07 14:51:47 -0300

    Update certificadoPaquimetro.jsx

--- src/components/certificados/paquimetro/certificadoPaquimetro.jsx
+++ src/components/certificados/paquimetro/certificadoPaquimetro.jsx
@@ -16,12 +16,12 @@ export default function CertPaquimetro() {
                 <div className="flex flex-row justify-between p-6">
                   <img  className="w-60 h-40" src={senai}/>
                     <div className="static mr-5  w-[70%]">
-                      <h1 className="text-black text-center font-bold text-6xl">CERTIFICADO DE CALIBRAÇÃO</h1>
-                      <h1 className="text-black text-center font-bold text-3xl">LABORATÓRIO DE CALIBRAÇÃO SUICLAB</h1>
-                      <h1 className="text-black text-center font-bold text-2xl">Rede Brasileira de Calibração </h1>
-                      <h1 className="text-black text-center font-bold text-2xl">Laboratório de Calibração Acreditado pela Cgcre</h1>
-                      <h1 className="text-black text-center font-bold text-2xl"> de acordo com a ABNT NBR ISO/IEC 17025, sob o número CAL 158</h1>
-                    </div>
+                        <h1 className="text-black text-center tracking-tighter font-bold font-serif text-6xl"> CERTIFICADO DE CALIBRAÇÃO </h1>
+                        <h1 className="text-black text-center font-serif font-bold text-2xl"> LABORATÓRIO DE CALIBRAÇÃO SUICLAB </h1>
+                        <h1 className="text-black text-center font-semibold text-2xl"> Rede Brasileira de Calibração</h1>
+                        <h1 className="text-black text-center font-semibold text-2xl"> Laboratório de Calibração Acreditado pela Cgcre </h1>
+                        <h1 className="text-black text-center font-semibold text-2xl"> de acordo com a ABNT NBR ISO/IEC 17025, sob o número CAL 158 </h1>
+                      </div>
                     <img className="h-52" src={selo}/>
                 </div>
 
